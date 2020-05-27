81df6196b923bc56f00a2097d69cb77613fbede5
Kay Lee <seoulkay@gmail.com>
2020-05-27 19:20:55 +0900

    Updates

---
+++ precache-manifest.8b733eae6cb539c4e2ba7347e275e951.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bfd3e9a31df2eb1f45749ee32913699",
+    "url": "/catcal/index.html"
+  },
+  {
+    "revision": "2aabae2f12a6728df67d",
+    "url": "/catcal/static/css/2.218b1d2b.chunk.css"
+  },
+  {
+    "revision": "e363e5e45952e6b930fc",
+    "url": "/catcal/static/css/main.e30f3588.chunk.css"
+  },
+  {
+    "revision": "2aabae2f12a6728df67d",
+    "url": "/catcal/static/js/2.f66a5626.chunk.js"
+  },
+  {
+    "revision": "7b5438a3f30ed2f44a09454b80873db3",
+    "url": "/catcal/static/js/2.f66a5626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e363e5e45952e6b930fc",
+    "url": "/catcal/static/js/main.891022cc.chunk.js"
+  },
+  {
+    "revision": "b070a2dcaae9d6c3d292",
+    "url": "/catcal/static/js/runtime-main.038899b5.js"
+  }
+]);
